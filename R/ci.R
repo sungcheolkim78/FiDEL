eea7968f678db2eacdbed77c5bdc040d20598010
Sungcheol Kim <kimsung@us.ibm.com>
2020-03-27 07:22:52 -0400

    update ci.R, ensemble.R, pcr.R

--- R/ci.R
+++ R/ci.R
@@ -4,17 +4,17 @@
 # 2020/02/14
 #
 # version 1.0
+# version 1.1 - using normalized beta, mu
 
 # Sampling Methods
-
-countXY <- function(classMatrix, seed=1) {
+countXY <- function(pcr_m, seed=1) {
   set.seed(seed)
-  N <- length(classMatrix$prob)
+  N <- length(pcr_m$prob)
   dices <- runif(N)
-  check <- (classMatrix$prob > dices)             # make class 1 or 2 from class probability
+  check <- (pcr_m$prob > dices)             # make class 1 or 2 from class probability
 
-  r1 <- sample(classMatrix$rank[check], 1)        # choose a rank from class 1
-  r2 <- sample(classMatrix$rank[!check], 1)       # choose a rank from class 2
+  r1 <- sample(pcr_m$rank[check], 1)        # choose a rank from class 1
+  r2 <- sample(pcr_m$rank[!check], 1)       # choose a rank from class 2
 
   # check conditions for P_xy
   if (r1 < r2) {    # rank of class 1 is less than that of class 2
@@ -24,14 +24,14 @@ countXY <- function(classMatrix, seed=1) {
   }
 }
 
-countXXY <- function(classMatrix, seed=1) {
+countXXY <- function(pcr_m, seed=1) {
   set.seed(seed)
-  N <- length(classMatrix$prob)
+  N <- length(pcr_m$prob)
   dices <- runif(N)
-  check <- (classMatrix$prob > dices)             # make class 1 or 2 from class probability
+  check <- (pcr_m$prob > dices)             # make class 1 or 2 from class probability
 
-  r1 <- sample(classMatrix$rank[check], 2)        # choose a rank from class 1
-  r2 <- sample(classMatrix$rank[!check], 1)       # choose a rank from class 2
+  r1 <- sample(pcr_m$rank[check], 2)        # choose a rank from class 1
+  r2 <- sample(pcr_m$rank[!check], 1)       # choose a rank from class 2
 
   # check conditions for P_xxy
   if (max(r1) < r2) {                   # rank of class 2 is the highest rank
@@ -41,14 +41,14 @@ countXXY <- function(classMatrix, seed=1) {
   }
 }
 
-countXYY <- function(classMatrix, seed=1) {
+countXYY <- function(pcr_m, seed=1) {
   set.seed(seed)
-  N <- length(classMatrix$prob)
+  N <- length(pcr_m$prob)
   dices <- runif(N)
-  check <- (classMatrix$prob > dices)             # make class 1 or 2 from class probability
+  check <- (pcr_m$prob > dices)             # make class 1 or 2 from class probability
 
-  r1 <- sample(classMatrix$rank[check], 1)        # choose a rank from class 1
-  r2 <- sample(classMatrix$rank[!check], 2)       # choose a rank from class 2
+  r1 <- sample(pcr_m$rank[check], 1)        # choose a rank from class 1
+  r2 <- sample(pcr_m$rank[!check], 2)       # choose a rank from class 2
 
   # check conditions for P_xyy
   if (r1 < min(r2)) {                  # rank of class 1 is the lowest rank
@@ -58,10 +58,9 @@ countXYY <- function(classMatrix, seed=1) {
   }
 }
 
-# Random sampling
-auc.Pxysample <- function(classMatrix, iter=5000, debug.flag=FALSE) {
+auc.Pxysample <- function(pcr_m, iter=5000, debug.flag=FALSE) {
   iters <- sample(1:iter)
-  res <- purrr::map_dbl(iters, ~countXY(classMatrix, seed=.x))
+  res <- purrr::map_dbl(iters, ~countXY(pcr_m, seed=.x))
   res <- cumsum(res)/(1:iter)
   #print(res[length(res)])
   if (debug.flag) plot(res)
@@ -69,9 +68,9 @@ auc.Pxysample <- function(classMatrix, iter=5000, debug.flag=FALSE) {
   return (res[length(res)])
 }
 
-Pxxy.sample <- function(classMatrix, iter=5000, debug.flag=FALSE) {
+Pxxy.sample <- function(pcr_m, iter=5000, debug.flag=FALSE) {
   iters <- sample(1:iter)
-  res <- purrr::map_dbl(iters, ~countXXY(classMatrix, seed=.x))
+  res <- purrr::map_dbl(iters, ~countXXY(pcr_m, seed=.x))
   res <- cumsum(res)/(1:iter)
 
   if (debug.flag) plot(res)
@@ -79,9 +78,9 @@ Pxxy.sample <- function(classMatrix, iter=5000, debug.flag=FALSE) {
   return (res[length(res)])
 }
 
-Pxyy.sample <- function(classMatrix, iter=5000, debug.flag=FALSE) {
+Pxyy.sample <- function(pcr_m, iter=5000, debug.flag=FALSE) {
   iters <- sample(1:iter)
-  res <- purrr::map_dbl(iters, ~countXYY(classMatrix, seed=.x))
+  res <- purrr::map_dbl(iters, ~countXYY(pcr_m, seed=.x))
   res <- cumsum(res)/(1:iter)
 
   if (debug.flag) plot(res)
@@ -90,16 +89,16 @@ Pxyy.sample <- function(classMatrix, iter=5000, debug.flag=FALSE) {
 }
 
 # Summation of class probability at rank
-auc.Pxysum <- function(classMatrix, debug.flag=FALSE) {
-  check <- c("rank", "prob") %in% names(classMatrix)
+auc.Pxysum <- function(pcr_m, debug.flag=FALSE) {
+  check <- c("rank", "prob") %in% names(pcr_m)
   stopifnot(all(check))
 
-  probs <- classMatrix[order(classMatrix$rank, decreasing = FALSE), "prob"]
+  probs <- pcr_m[order(pcr_m$rank, decreasing = FALSE), "prob"]
   res <- outer(probs, 1 - probs, "*")
   idx <- upper.tri(res)
 
-  N <- length(classMatrix$prob)
-  N1 <- sum(classMatrix$prob)
+  N <- length(pcr_m$prob)
+  N1 <- sum(pcr_m$prob)
   N2 <- N - N1
 
   if (debug.flag) {
@@ -110,13 +109,13 @@ auc.Pxysum <- function(classMatrix, debug.flag=FALSE) {
   return(sum(res[idx])/(N1*N2))
 }
 
-Pxxy.sum <- function(classMatrix, debug.flag=FALSE) {
-  check <- c("rank", "prob") %in% names(classMatrix)
+Pxxy.sum <- function(pcr_m, debug.flag=FALSE) {
+  check <- c("rank", "prob") %in% names(pcr_m)
   stopifnot(all(check))
 
   res <- 0
-  N <- length(classMatrix$prob)
-  probs <- classMatrix[order(classMatrix$rank), "prob"]
+  N <- length(pcr_m$prob)
+  probs <- pcr_m[order(pcr_m$rank), "prob"]
 
   for (i in 1:N) {
     for (j in 1:N) {
@@ -128,7 +127,7 @@ Pxxy.sum <- function(classMatrix, debug.flag=FALSE) {
     }
   }
 
-  N1 <- sum(classMatrix$prob)
+  N1 <- sum(pcr_m$prob)
   N2 <- N - N1
 
   if (debug.flag) {
@@ -139,13 +138,13 @@ Pxxy.sum <- function(classMatrix, debug.flag=FALSE) {
   return(res/(N1*N1*N2))
 }
 
-Pxxy.sum2 <- function(classMatrix, debug.flag=FALSE) {
-  check <- c("rank", "prob") %in% names(classMatrix)
+Pxxy.sum2 <- function(pcr_m, debug.flag=FALSE) {
+  check <- c("rank", "prob") %in% names(pcr_m)
   stopifnot(all(check))
 
   res <- 0
-  N <- length(classMatrix$prob)
-  probs <- classMatrix[order(classMatrix$rank), "prob"]
+  N <- length(pcr_m$prob)
+  probs <- pcr_m[order(pcr_m$rank), "prob"]
   r2r3 <- outer(probs, 1 - probs, "*")
 
   for (i in 1:N) {
@@ -155,7 +154,7 @@ Pxxy.sum2 <- function(classMatrix, debug.flag=FALSE) {
     res <- res + probs[i] * sum(r2r3[idx])
   }
 
-  N1 <- sum(classMatrix$prob)
+  N1 <- sum(pcr_m$prob)
   N2 <- N - N1
 
   if (debug.flag) {
@@ -166,12 +165,12 @@ Pxxy.sum2 <- function(classMatrix, debug.flag=FALSE) {
   return(res/(N1*N1*N2))
 }
 
-Pxyy.sum <- function(classMatrix, debug.flag=FALSE) {
-  check <- c("rank", "prob") %in% names(classMatrix)
+Pxyy.sum <- function(pcr_m, debug.flag=FALSE) {
+  check <- c("rank", "prob") %in% names(pcr_m)
   stopifnot(all(check))
 
-  N <- length(classMatrix$prob)
-  probs <- classMatrix[order(classMatrix$rank), "prob"]
+  N <- length(pcr_m$prob)
+  probs <- pcr_m[order(pcr_m$rank), "prob"]
 
   pxyy <- function(i, j, k) {
     if (i < min(c(j,k))) {
@@ -181,19 +180,19 @@ Pxyy.sum <- function(classMatrix, debug.flag=FALSE) {
 
   res <- purrr::reduce(expand.grid(1:N, 1:N, 1:N), ~pxyy(..1, ..2, ..3))
 
-  N1 <- sum(classMatrix$prob)
+  N1 <- sum(pcr_m$prob)
   N2 <- N - N1
 
   return(res/(N1*N2*N2))
 }
 
-Pxyy.sum2 <- function(classMatrix, debug.flag=FALSE) {
-  check <- c("rank", "prob") %in% names(classMatrix)
+Pxyy.sum2 <- function(pcr_m, debug.flag=FALSE) {
+  check <- c("rank", "prob") %in% names(pcr_m)
   stopifnot(all(check))
 
   res <- 0
-  N <- length(classMatrix$prob)
-  probs <- classMatrix[order(classMatrix$rank), "prob"]
+  N <- length(pcr_m$prob)
+  probs <- pcr_m[order(pcr_m$rank), "prob"]
   r2r3 <- outer(1 - probs, 1 - probs, "*")
 
   for (i in 1:N) {
@@ -205,7 +204,7 @@ Pxyy.sum2 <- function(classMatrix, debug.flag=FALSE) {
     res <- res + probs[i] * sum(r2r3[idx])
   }
 
-  N1 <- sum(classMatrix$prob)
+  N1 <- sum(pcr_m$prob)
   N2 <- N - N1
 
   if (debug.flag) {
@@ -216,19 +215,20 @@ Pxyy.sum2 <- function(classMatrix, debug.flag=FALSE) {
   return(res/(N1*N2*N2))
 }
 
-var.auc <- function(classMatrix, method='Sampling', debug.flag=FALSE) {
-  N <- length(classMatrix$prob)
-  N1 <- sum(classMatrix$prob)
+# Confidence interval calculation
+var.auc <- function(pcr_m, method='Sampling', debug.flag=FALSE) {
+  N <- length(pcr_m$prob)
+  N1 <- sum(pcr_m$prob)
   N2 <- N - N1
 
   if (method == 'Sampling') {
-    Pxxy_value <- Pxxy.sample(classMatrix)
-    Pxyy_value <- Pxyy.sample(classMatrix)
-    Pxy_value <- auc.Pxysample(classMatrix)
+    Pxxy_value <- Pxxy.sample(pcr_m)
+    Pxyy_value <- Pxyy.sample(pcr_m)
+    Pxy_value <- auc.Pxysample(pcr_m)
   } else if (method == 'Sum') {
-    Pxxy_value <- Pxxy.sum2(classMatrix)
-    Pxyy_value <- Pxyy.sum2(classMatrix)
-    Pxy_value <- auc.Pxysum(classMatrix)
+    Pxxy_value <- Pxxy.sum2(pcr_m)
+    Pxyy_value <- Pxyy.sum2(pcr_m)
+    Pxy_value <- auc.Pxysum(pcr_m)
   }
   var_auc <- (Pxy_value*(1-Pxy_value) +
                 (N1 - 1)*(Pxxy_value - Pxy_value*Pxy_value) +
@@ -246,25 +246,35 @@ var.auc <- function(classMatrix, method='Sampling', debug.flag=FALSE) {
   return(var_auc)
 }
 
-var_auc_fermi <- function(auc, N1, N2, iter=5000, debug.flag=FALSE) {
-  N <- N1 + N2
-  l <- lambda.auc(auc, N=N, rho=N1/N)
-  if (debug.flag) print(l)
+var_auc_fermi <- function(auc, rho, N=1000, iter=5000, method='sampling', debug.flag=FALSE) {
+  # get FD variables
+  b <- get_fermi(auc, rho, N=N)
+  N1 <- floor(N*rho)
+  N2 <- N - N1
 
-  ranks <- 1:N
-  probs <- fermi(ranks, l['l1'], l['l2'])
-  if (debug.flag) plot(ranks, probs)
-  cm <- data.frame(rank=ranks, prob=probs)
+  if (method == 'sampling') {
+    ranks <- seq(1, N)
+    probs <- fermi.b(ranks, b[[1]], b[[2]])
+    #if (debug.flag) plot(ranks, probs)
+    cm <- data.frame(rank=ranks, prob=probs)
 
-  Pxxy_value <- Pxxy.sample(cm, iter=iter)
-  Pxyy_value <- Pxyy.sample(cm, iter=iter)
-  Pxy_value <- auc.Pxysample(cm, iter=iter)
+    Pxxy_value <- Pxxy.sample(cm, iter=iter)
+    Pxyy_value <- Pxyy.sample(cm, iter=iter)
+    Pxy_value <- auc.Pxysample(cm, iter=iter)
+  } else {
+    Pxxy_value <- Pxxy_int(b[[1]]*N, b[[2]]/N, rho)
+    Pxyy_value <- Pxyy_int(b[[1]]*N, b[[2]]/N, rho)
+    Pxy_value <- Pxy_int(b[[1]]*N, b[[2]]/N, rho)
+  }
+
   var_auc <- (Pxy_value*(1-Pxy_value) +
                 (N1 - 1)*(Pxxy_value - Pxy_value*Pxy_value) +
                 (N2 - 1)*(Pxyy_value - Pxy_value*Pxy_value))/(N1*N2)
 
   if (debug.flag) {
-    print(paste0('Rho: ', N1/N))
+    print(paste0("Method: ", method))
+    print(paste0("beta': ", b[[1]]*N))
+    print(paste0("mu': ", b[[2]]/N))
     print(paste0('Pxxy: ', Pxxy_value))
     print(paste0('Pxyy: ', Pxyy_value))
     print(paste0('Pxy: ', Pxy_value))
@@ -272,5 +282,46 @@ var_auc_fermi <- function(auc, N1, N2, iter=5000, debug.flag=FALSE) {
     print(paste0('AUC Sigma: ', sqrt(var_auc)))
     print(paste0('95% CI: ', auc - 1.96*sqrt(var_auc), ' - ', auc + 1.96*sqrt(var_auc)))
   }
-  return(var_auc)
+
+  return(c(var_auc=var_auc, Pxy=Pxy_value, Pxxy=Pxxy_value, Pxyy=Pxyy_value))
+}
+
+# Integral method
+Pxy_int <- function(beta, mu, rho, resol=1e-6) {
+  tic(sprintf('... Pxy integral calculation with res = %.3g', resol))
+  ptable <- fermi.b(seq(0,1,by=resol), beta, mu)
+
+  A <- ptable
+  B <- rev(seq_along(ptable)) - rev(cumsum(rev(ptable)))
+  res <- sum(A * B) - sum(ptable * (1-ptable))
+  res <- res * resol * resol / (rho*(1-rho))
+  toc()
+
+  return(res)
+}
+
+Pxxy_int <- function(beta, mu, rho, resol=1e-6) {
+  tic(sprintf('... Pxxy integral calculation with res = %.3g', resol))
+  ptable <- fermi.b(seq(0,1,by=resol), beta, mu)
+
+  A <- 2*ptable*cumsum(ptable) - ptable^2
+  B <- rev(seq_along(ptable)) - rev(cumsum(rev(ptable)))
+  res <- sum(A * B) - sum(ptable^2 * (1-ptable)) - sum(ptable^2) - 2*sum(ptable*(1-ptable))
+  res <- res * resol^3 / (rho^2 * (1-rho))
+  toc()
+
+  return(res)
+}
+
+Pxyy_int <- function(beta, mu, rho, resol=1e-6) {
+  tic(sprintf('... Pxyy integral calculation with res = %.3g', resol))
+  ptable <- fermi.b(seq(0,1,by=resol), beta, mu)
+
+  A <- ptable
+  B <- rev(cumsum(rev(1-ptable)))^2
+  res <- sum(A * B) - sum(ptable * (1-ptable)^2) - sum((1-ptable)^2) - 2*sum(ptable*(1-ptable))
+  res <- res * resol^3 / (rho * (1-rho)^2)
+  toc()
+
+  return(res)
 }
--- R/ensemble.R
+++ R/ensemble.R
@@ -4,44 +4,54 @@
 # 2020/03/10
 #
 # version 1.0
+# version 1.1 - revision with beta and mu
+# version 1.2 - 2020/3/26 - correlation plot
 
 library(ggplot2)
 library(ggpubr)
+library(tictoc)
 
 setClass("FDensemble",
          representation(predictions = "matrix",
+                        logit_matrix = "matrix",
                         rank_matrix = "matrix",
-                        lambda1s = "numeric",
-                        lambda2s = "numeric",
-                        rstars = "numeric",
+                        beta = "numeric",
+                        mu = "numeric",
+                        rstar = "numeric",
                         nsamples = "numeric",
                         nmethods = "numeric",
-                        estimated_label = "numeric",
-                        actual_label = "numeric",
                         ensemble_auc = "numeric",
                         actual_performance = "numeric",
+                        actual_prevalence = "numeric",
+                        actual_label = "numeric",
                         estimated_performance = "numeric",
                         estimated_prevalence = "numeric",
-                        actual_prevalence = "numeric",
-                        estimated_rank = "numeric"))
+                        estimated_logit = "numeric",
+                        estimated_rank = "numeric",
+                        estimated_label = "factor",
+                        estimated_prob = "numeric"
+                        ))
 
-setMethod("initialize", "FDensemble", function(.Object, predictions) {
-  cat("... FDensemble initializer \n")
+setMethod("initialize", "FDensemble", function(.Object, predictions, ...) {
+  #cat("... FDensemble initializer \n")
   .Object@predictions <- predictions
   .Object@nsamples <- nrow(predictions)
   .Object@nmethods <- ncol(predictions)
-
-
+  .Object@logit_matrix <- apply(predictions, 2, frankv)
+  .Object@rank_matrix <- apply(predictions, 2, frankv)
+  .Object@estimated_label <- as.factor(c('class1', rep('class2', .Object@nsamples-1)))
 
   return(.Object)
 })
 
-fde <- fdensemble <- function(predictions, actual_performance) {
-  cat("... FDensemble constructor \n")
-  if (missing(actual_performance)) { new (Class="FDensemble", predictions = predictions) }
-  else {
-    obj <- new (Class="FDensemble", predictions = predictions, actual_performance = actual_performance)
-    calculate_performance(obj, actual_performance)
+fde <- fdensemble <- function(predictions, actual_label) {
+  obj <- new (Class="FDensemble", predictions = predictions)
+  if (!missing(actual_label)) {
+    message('... FDensemble with actual_label')
+    calculate_performance(obj, actual_label)
+  } else {
+    message('... FDensemble with only predictions')
+    obj
   }
 }
 
@@ -58,33 +68,40 @@ setValidity("FDensemble", function(object) {
   }
 })
 
-setGeneric("calculate_performance", function(.Object, actual_labels) {standardGeneric("calculate_performance")})
+setGeneric("calculate_performance", function(.Object, actual_label, alpha=1) {standardGeneric("calculate_performance")})
 
-setMethod("calculate_performance", "FDensemble", function(.Object, actual_labels) {
+setMethod("calculate_performance", "FDensemble", function(.Object, actual_label, alpha=1) {
+  tic(sprintf('... N:%d, M:%d', .Object@nsamples, .Object@nmethods))
   # save label data
-  .Object@actual_label <- actual_labels
+  .Object@actual_label <- actual_label
 
   # calculate auc using labels
-  .Object@actual_performance <- apply(.Object@predictions, 2, auc.rank, actual_labels)
+  .Object@actual_performance <- apply(.Object@predictions, 2, auc.rank, actual_label)
 
-  # calculate lambda1, 2, and rstar
-  .Object@actual_prevalence <- attr(actual_labels, 'rho')
-  l_list <- sapply(.Object@actual_performance, lambda.auc, N=.Object@nsamples, rho=.Object@actual_prevalence)
-  .Object@lambda1s <- as.vector(l_list[1, ])
-  .Object@lambda2s <- as.vector(l_list[2, ])
-  .Object@rstars <- as.vector(l_list[3, ])
+  # calculate beta, mu and rstar
+  .Object@actual_prevalence <- attr(actual_label, 'rho')
+  b_list <- sapply(.Object@actual_performance, get_fermi, rho=.Object@actual_prevalence)
+  .Object@beta <- as.vector(b_list[1, ])
+  .Object@mu <- as.vector(b_list[2, ])
+  .Object@rstar <- as.vector(b_list[3, ])
 
   # calculate new rank scores
-  res <- matrix(0, nrow=.Object@nsamples, ncol=.Object@nmethods)
+  colnames(.Object@logit_matrix) <- colnames(.Object@predictions)
+  .Object@logit_matrix <- apply(.Object@predictions, 2, frankv)/.Object@nsamples
   for (m in seq(1, .Object@nmethods)) {
-    res[ , m] <- rank(.Object@predictions[, m])
-    res[ , m] <- .Object@lambda2s[m] *(.Object@rstars[m] - res[, m])
+    .Object@logit_matrix[, m] <- .Object@beta[m]^alpha *(.Object@rstar[m] - .Object@logit_matrix[, m])
   }
-  .Object@rank_matrix <- res
-  .Object@estimated_rank <- rowSums(res)
+  .Object@estimated_rank <- rowSums(.Object@logit_matrix)
+  .Object@estimated_label[.Object@estimated_rank > 0] <- 'class1'
+  .Object@estimated_prob <- 1/(1+exp(-.Object@estimated_rank))
 
   # calculate ensemble AUC
-  .Object@ensemble_auc <- auc.rank(.Object@estimated_rank, actual_labels)
+  .Object@ensemble_auc <- auc.rank(.Object@estimated_rank, actual_label)
+  cat('... Ensemble AUC:', .Object@ensemble_auc, '\n')
+  cat('... AUC list:', .Object@actual_performance, '\n')
+  cat('... beta list:', .Object@beta, '\n')
+  cat('... mu list:', .Object@mu, '\n')
+  toc()
 
   return(.Object)
 })
@@ -92,24 +109,41 @@ setMethod("calculate_performance", "FDensemble", function(.Object, actual_labels
 setGeneric("predict_performance", function(.Object, actual_performance, p, alpha=1) {standardGeneric("predict_performance")})
 
 setMethod("predict_performance", "FDensemble", function(.Object, actual_performance, p, alpha=1) {
+  tic(sprintf('... N:%d, M:%d', .Object@nsamples, .Object@nmethods))
   # calculate auc using labels
   .Object@actual_performance <- actual_performance
+  gen_name <- function(x) { paste0('A_', round(x, digits=3)) }
+  classifier_names <- sapply(actual_performance, gen_name)
+  colnames(.Object@predictions) <- classifier_names
 
-  # calculate lambda1, 2, and rstar
+  # calculate beta, mu and rstar
   .Object@actual_prevalence <- p
-  l_list <- sapply(.Object@actual_performance, get_fermi, .Object@actual_prevalence)
-  .Object@lambda1s <- as.vector(-l_list[1, ]*l_list[2, ])
-  .Object@lambda2s <- as.vector(l_list[1, ]/.Object@nsamples)
-  .Object@rstars <- as.vector(l_list[3, ]*.Object@nsamples)
+  b_list <- sapply(actual_performance, get_fermi, rho=p)
+  .Object@beta <- as.vector(b_list[1, ])
+  .Object@mu <- as.vector(b_list[2, ])
+  .Object@rstar <- as.vector(b_list[3, ])
 
   # calculate new rank scores
-  res <- matrix(0, nrow=.Object@nsamples, ncol=.Object@nmethods)
+  colnames(.Object@logit_matrix) <- colnames(.Object@predictions)
+  .Object@logit_matrix <- apply(.Object@predictions, 2, frankv)/.Object@nsamples
   for (m in seq(1, .Object@nmethods)) {
-    res[ , m] <- rank(.Object@predictions[, m])
-    res[ , m] <- .Object@lambda2s[m]^alpha *(.Object@rstars[m] - res[, m])
+    .Object@logit_matrix[, m] <- .Object@beta[m]^alpha * (.Object@rstar[m] - .Object@logit_matrix[, m] )
   }
-  .Object@rank_matrix <- res
-  .Object@estimated_rank <- rowSums(res)
+  .Object@estimated_logit <- -rowMeans(.Object@logit_matrix)
+  .Object@estimated_label[.Object@estimated_logit > 0] <- 'class1'
+  .Object@estimated_prob <- 1/(1+exp(-.Object@estimated_logit))
+  .Object@estimated_rank <- frankv(.Object@estimated_logit)
+
+  if (ncol(.Object@rank_matrix) != ncol(.Object@predictions)) {
+    .Object@rank_matrix <- .Object@rank_matrix[, -ncol(.Object@rank_matrix)]
+  }
+  colnames(.Object@rank_matrix) <- classifier_names
+  .Object@rank_matrix <- cbind(.Object@rank_matrix, A_FD=.Object@estimated_rank)
+
+  cat('... AUC list:', .Object@actual_performance, '\n')
+  cat('... beta list:', .Object@beta, '\n')
+  cat('... mu list:', .Object@mu, '\n')
+  toc()
 
   return(.Object)
 })
@@ -117,23 +151,51 @@ setMethod("predict_performance", "FDensemble", function(.Object, actual_performa
 setGeneric("plot_FDstatistics", function(.Object) {standardGeneric("plot_FDstatistics")})
 
 setMethod("plot_FDstatistics", "FDensemble", function(.Object) {
-    df <- data.frame(x=.Object@actual_performance, l1=.Object@lambda1s, l2=.Object@lambda2s, rs=.Object@rstars)
-    g1 <- ggplot(data = df) + geom_point(aes(x=x, y=l1)) + xlab('AUC') + ylab('Lambda_1') + theme_classic()
-    g2 <- ggplot(data = df) + geom_point(aes(x=x, y=l2)) + xlab('AUC') + ylab('Lambda_2') + theme_classic()
+    df <- data.frame(x=.Object@actual_performance, b=.Object@beta, m=.Object@mu, rs=.Object@rstar)
+    g1 <- ggplot(data = df) + geom_point(aes(x=x, y=b)) + xlab('AUC') + ylab('beta') + theme_classic()
+    g2 <- ggplot(data = df) + geom_point(aes(x=x, y=m)) + xlab('AUC') + ylab('mu') + theme_classic()
     g3 <- ggplot(data = df) + geom_point(aes(x=x, y=rs)) + xlab('AUC') + ylab('R_star') + theme_classic()
 
     g <- ggarrange(g1, g2, g3, labels=c("A", "B", "C"), ncol=3  , nrow=1)
     print(g)
 })
 
+setGeneric("plot_cor", function(.Object, filename='cor.pdf', ...) {standardGeneric("plot_cor")})
+
+setMethod("plot_cor", "FDensemble", function(.Object, filename='cor.pdf', legend_flag=FALSE) {
+  cor_m <- as.data.table(cor(.Object@rank_matrix))
+  cor_m$Var1 <- colnames(.Object@rank_matrix)
+  melted_cor_m <- melt(cor_m, id='Var1', variable.name = 'Var2')
+
+  g <- ggplot(melted_cor_m, aes(Var1, Var2)) +
+    geom_tile(aes(fill = value), colour = "white") +
+    scale_fill_gradient(low = "white", high = "steelblue") +
+    theme_grey(base_size = 9) + labs(x = "", y = "") +
+    scale_x_discrete(expand = c(0, 0)) +
+    scale_y_discrete(expand = c(0, 0))
+  if (!legend_flag) {
+    g <- g + theme(legend.position = "none")
+  }
+
+  ggsave(filename, g, width=8, height=7)
+  print(g)
+})
+
 setGeneric("plot_single", function(.Object, target, ...) {standardGeneric("plot_single")})
 
 setMethod("plot_single", "FDensemble", function(.Object, target, c, n=100, m=100) {
-  if (missing(c)) { c <- 1 }
+  if (missing(c)) { c <- 0 }
   if (c > .Object@nmethods) { c <- .Object@nmethods }
+  if (c < 0) { c <- 0 }
 
   if (target == 'score') {
-    g <- plot.scores(.Object@predictions[ , c], .Object@actual_label)
+    if (c == 0) { scores <- .Object@estimated_logit }
+    else { scores <- .Object@predictions[ , c] }
+    if (length(.Object@actual_label) > 0) {
+      g <- plot.scores(scores, .Object@actual_label)
+    } else {
+      g <- plot.scores(scores)
+    }
     print(g)
   }
 
